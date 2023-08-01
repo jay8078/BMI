09c08ce1d885e50eb0c6f0fff2e1a021cab9a144
jay8078 <112859480+jay8078@users.noreply.github.com>
2023-08-01 17:26:24 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,10 +2,16 @@
 
 export VAR=/home/dev/text1
 
+echo ${VAR}
+
 echo 'VARI = "/home/dev/text2"' >> /etc/environment
 
 source /etc/environment
 
+echo ${VARI}
+
 echo 'VARIA= "/home/dev/text3"' >> ~/.bashrc
 
 source ~/.bashrc
+
+echo ${VARIA}
